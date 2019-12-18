2e87b03e901df2b4ebb941e3bb51c8c7b2ecf52f
leorodriguessilva <leonardo_faust@hotmail.com>
2019-12-18 00:28:38 -0300

    created fake geocode service for testing reasons

---
+++ geocode/FakeGeocodeService.js
@@ -0,0 +1,29 @@
+class FakeGeocodeService {
+
+    constructor(randomLocationMaxSize) {
+        if(!randomLocationMaxSize) {
+            randomLocationMaxSize = 1;
+        }
+        this.randomLocationMaxSize = randomLocationMaxSize;
+    }
+
+    async geocode(query) {
+        let results = [];
+        let amountOfLocations = Math.random() * (this.randomLocationMaxSize - 1) + 1;
+        for(var i = 0; i < amountOfLocations; i++) {
+            results.push(_generateRandomLatLon());
+        }
+        return results;
+    }
+
+    _generateRandomLatLng() {  
+        var lat = Math.random() * (180 - 1) + 1;
+        var lng = Math.random() * (180 - 1) + 1;
+        return {
+            lat,
+            lng
+        }
+    }
+}
+
+module.exports = FakeGeocodeService;
