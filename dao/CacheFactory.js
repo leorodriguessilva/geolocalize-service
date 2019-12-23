67bcf312559b645d930c8a8af15130caa042bebd
Leonardo Rodrigues da Silva <leonardo_faust@hotmail.com>
2019-12-23 01:07:04 -0300

    created cache factory

--- dao/CacheFactory.js
+++ dao/CacheFactory.js
@@ -1,7 +1,41 @@
+const NoGeolocalizationApiResultCache = require('./NoGeolocalizationApiResultCache');
+const RedisGeolocalizationApiResultCache = require('./RedisGeolocalizationApiResultCache');
+const TypeCacheEnum = require('../domain/TypeCache');
+
 class CacheFactory {
 
     constructor(typeCache) {
-        
+        this.typeCache = typeCache;
+    }
+
+    create() {
+        if (this.typeCache == TypeCacheEnum.MEMORY_AND_PERSISTENT_CACHE 
+            || this.typeCache == TypeCacheEnum.ONLY_MEMORY_CACHE) {
+            return this._createCachedAccess();
+        }
+        return this._createNoCachedAccess();
+    }
+
+    _createNoCachedAccess() {
+        if (this.geolocalizationApiResultCache) {
+            return this.geolocalizationApiResultCache;
+        }
+        this.geolocalizationApiResultCache = new NoGeolocalizationApiResultCache();
+        return this.geolocalizationApiResultCache;
+    }
+
+    _createCachedAccess() {
+        if (this.geolocalizationApiResultCache) {
+            return this.geolocalizationApiResultCache
+        }
+        this.geolocalizationApiResultCache = new RedisGeolocalizationApiResultCache(
+            this.environmentConfig.cacheServerAddress, 
+            this.environmentConfig.cacheServerPort, 
+            this.environmentConfig.cacheServerPass,
+            this.environmentConfig.cacheExpirationInSeconds);
+        return this.geolocalizationApiResultCache;
     }
 
 }
+
+module.exports = CacheFactory;
