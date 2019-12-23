78e0a7579be39c2213f6d8b7553e625e109fd20d
Leonardo Rodrigues da Silva <leonardo_faust@hotmail.com>
2019-12-23 01:11:36 -0300

    changed geolocalization cache result to use cache and dao factory

--- service/CacheGeolocalizationResultService.js
+++ service/CacheGeolocalizationResultService.js
@@ -1,21 +1,14 @@
-const TypeCacheEnum = require('../domain/TypeCache');
-const DynamoDBGeolocalizationApiResultDao = require('../dao/DynamoDBGeolocalizationApiResultDao');
-const CassandraGeolocalizationApiResultDao = require('../dao/CassandraGeolocalizationApiResultDao');
-const RedisGeolocalizationApiResultCache = require('../dao/RedisGeolocalizationApiResultCache');
-const GeolocalizationApiResultDaoLogger = require('../dao/GeolocalizationApiResultDaoLogger');
-const NoDatabaseGeolocalizationApiResultDao = require('../dao/NoDatabaseGeolocalizationApiResultDao');
-const NoGeolocalizationApiResultCache = require('../dao/NoGeolocalizationApiResultCache');
 const isBeforeNow = require('../util/Util');
 
 class CacheGeolocalizationResultService {
 
     constructor(
         environmentConfig,
-        typeCache) {
+        daoFactory,
+        cacheFactory) {
         this.environmentConfig = environmentConfig;
-        var connectionCreations = this.mapDatabaseAndCacheByType();
-        var daoCreationCallback = connectionCreations.get(typeCache);
-        daoCreationCallback(this);
+        this.geolocalizationApiResultDao = daoFactory.create();
+        this.geolocalizationApiResultCache = cacheFactory.create();
     }
 
     async saveCache(geolocalizationQuery, latlon) { 
@@ -66,68 +59,6 @@ class CacheGeolocalizationResultService {
         return expireAt.getTime();
     }
 
-    mapDatabaseAndCacheByType() {
-        var connectionCreations = new Map();
-        connectionCreations.set(TypeCacheEnum.NO_CACHE, this.createNoCachedDao);
-        connectionCreations.set(TypeCacheEnum.ONLY_MEMORY_CACHE, this.createMemoryCachedDao);
-        connectionCreations.set(TypeCacheEnum.ONLY_PERSISTENT_CACHE, this.createPersistentCachedDao);
-        connectionCreations.set(TypeCacheEnum.MEMORY_AND_PERSISTENT_CACHE, this.createMemoryAndPersistentCachedDao);
-        return connectionCreations;
-    }
-
-    createNoCachedDao(caller) {
-        console.log('Using no cache for this call');
-        caller.createNoCachedAccess();
-        caller.createNoPersistentDao();
-    }
-
-    createMemoryCachedDao(caller) {
-        console.log('Using only in memory cache for this call');
-        caller.createCachedAccess();
-        caller.createNoPersistentDao();
-    }
-
-    createPersistentCachedDao(caller) {
-        console.log('Using only in persistent cache for this call');
-        caller.createNoCachedAccess();
-        caller.createPersistentDao();
-    }
-
-    createMemoryAndPersistentCachedDao(caller) {
-        console.log('Using in memory and persistent cache for this call');
-        caller.createCachedAccess();
-        caller.createPersistentDao();
-    }
-
-    createNoPersistentDao() {
-        this.geolocalizationApiResultDao = new NoDatabaseGeolocalizationApiResultDao();
-    }
-
-    createNoCachedAccess() {
-        this.geolocalizationApiResultCache = new NoGeolocalizationApiResultCache();
-    }
-
-    createCachedAccess() {
-        this.geolocalizationApiResultCache = new RedisGeolocalizationApiResultCache(
-            this.environmentConfig.cacheServerAddress, 
-            this.environmentConfig.cacheServerPort, 
-            this.environmentConfig.cacheServerPass,
-            this.environmentConfig.cacheExpirationInSeconds);
-    }
-
-    createPersistentDao() {
-        let wrapped = new CassandraGeolocalizationApiResultDao(
-            this.environmentConfig.databaseServerAddress, 
-            this.environmentConfig.databaseUser, 
-            this.environmentConfig.databasePass,
-            this.environmentConfig.persistentCacheTable);
-        if (this.environmentConfig.useDynamoDB) {
-            wrapped = new DynamoDBGeolocalizationApiResultDao(
-                this.environmentConfig.AWS_DEFAULT_REGION, 
-                this.environmentConfig.persistentCacheTable);
-        }
-        this.geolocalizationApiResultDao = new GeolocalizationApiResultDaoLogger(wrapped);
-    }
 }
 
 module.exports = CacheGeolocalizationResultService;
