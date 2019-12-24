c128448aee94b5e1c5ed1dc8a17394a6a13bc8db
leorodriguessilva <leonardo_faust@hotmail.com>
2019-12-24 19:19:12 -0300

    create tests for date utils

---
+++ test/unit/util/DateUtil.test.js
@@ -0,0 +1,22 @@
+const isBeforeNow = require('../../../util/DateUtil');
+
+describe('function to detect if given time from date is before time from now', () => {
+
+    test('when using a date representing tomorrow the result should be falsy', () => {
+        const tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        expect(isBeforeNow(tomorrow)).toBeFalsy();
+    });
+
+    test('when using a date representing today the result should be falsy', () => {
+        const today = new Date();
+        expect(isBeforeNow(today)).toBeFalsy();
+    });
+
+    test('when using a date representing yesterday the result should be truthy', () => {
+        const yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+        expect(isBeforeNow(yesterday)).toBeTruthy();
+    });
+
+});
