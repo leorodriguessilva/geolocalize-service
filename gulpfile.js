52a040e9ab050a6b1b76c03c491c4ac45fa4e18c
Leonardo Rodrigues da Silva <leonardo_faust@hotmail.com>
2019-12-26 18:01:18 -0300

    fixing gulp tasks and added package.json inside gulp to write the name and version of the package

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,27 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
 const clean = require('gulp-clean');
+const package = require('./package.json');
  
-exports.clean = () => (
-    gulp.src('./dist', {read: false})
-    .pipe(clean())
-);
+const cleanDist = async () => {
+    await gulp.src('./dist', {read: false, allowEmpty: true})
+    .pipe(clean());
+};
 
-exports.default = () => (
-    gulp.src([
-        '!./test', 
-        '!./dist',
-        '!./.gitignore',
-        '!./.gulpfile.js',
-        '!./README.md',
-        '!.vscode/',
+const build = async () => {
+    await gulp.src([
         './**/*', 
+        '!./test/**', 
+        '!./dist/**',
+        '!./.gitignore',
+        '!gulpfile.js',
+        '!README.md',
+        '!./.vscode/**',
     ])
-        .pipe(zip('archive.zip'))
-        .pipe(gulp.dest('dist'))
-);
+    .pipe(zip(`${package.name}_${package.version}.zip`))
+    .pipe(gulp.dest('dist'));
+};
+
+module.exports.clean = cleanDist;
+
+module.exports.build = build;
